37fed2c7f13a7c3e0ff99e9533b1ad0748e6e9a4
NicholasLam1 <n2lam@ucsd.edu>
2022-10-14 19:55:31 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,11 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void averageWithoutLowest() {
+  double [] input = {1.0, 0.0, 3.0, 5.0 };
+  assertEquals(3.0, ArrayExamples.averageWithoutLowest(input), 0.0001);
+}
+
 }
